c654944dc5ba005f8600357485c37403dd7131c4
Jordan-type <jordanmuthemba25@gmail.com>
2020-04-12 09:52:38 +0300

    started with challenge 1-2-3

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,26 @@
-const covid19ImpactEstimator = (data) => data;
+import chain from "./index"
+
+const covid19ImpactEstimator = (data) => {
+    const estimator = chain(
+        // challenge one 
+        estimateCurrentlyInfected,
+        estimateProjectedInfction,
+
+        // challenge two
+        estimatedServereCases,
+        estimatedBedSpaceAvailablility,
+
+        // challenge three
+        estimatedCasesForICU,
+        estimatedCasesForVentilators,
+        estimateddollarsInFlight
+    );
+
+    return estimator({
+        data,
+        impact: {},
+        servereImpact:{}
+    });
+};
 
 export default covid19ImpactEstimator;
